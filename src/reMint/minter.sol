348ab56975d7670d014dd0a06e5c5620e6d48303
rysky-boi <dave@rysk.finance>
2025-03-01 13:29:29 +0000

    new design

--- src/reMint/minter.sol
+++ src/reMint/minter.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-import {IMintSpecific} from "./interfaces/mintSpecific.sol";
+import {
+    INonFungibleSeaDropToken
+} from "../interfaces/INonFungibleSeaDropToken.sol";
 
 import { IERC721A } from "lib/ERC721A/contracts/IERC721A.sol";
 
@@ -13,29 +15,37 @@ contract BurnToMintSeaDrop {
     /// @notice Address of the NFT contract to burn tokens from
     address public immutable burnTokenContract;
 
+    /// @notice Address of the NFT contract to mint tokens to
+    address public immutable nftContract;
+
     /// @notice Mapping to track if a token has been minted using this mechanism
     mapping(uint256 => bool) public tokenIdRedeemed;
 
     /// @notice Event emitted when a token is minted
     event SeaDropMint(
         address indexed nftContract,
-        address indexed minter
+        address indexed minter,
+        uint256 tokenId
     );
 
     /// @dev Constructor to set the burn token contract address
-    constructor(address _burnTokenContract) {
+    constructor(address _burnTokenContract, address  _nftContract) {
+        nftContract = _nftContract;
         burnTokenContract = _burnTokenContract;
+    }
+
+    function mintAll(
+    ) internal {
+        //This will mint all the tokens to the contract address
+        INonFungibleSeaDropToken(nftContract).mintSeaDrop(address(this), IERC721A(nftContract).totalSupply());
     }
 
     /**
      * @notice Mint by burning a specific NFT
      * @dev This is the only mint method that will be enabled
-     *
-     * @param nftContract      The nft contract to mint from
      * @param burnTokenId      The ID of the token to burn
      */
     function mintByBurning(
-        address nftContract,
         uint256 burnTokenId
     ) external {
         // Check that the token hasn't already been redeemed
@@ -55,13 +65,12 @@ contract BurnToMintSeaDrop {
         // Mark the token as redeemed
         tokenIdRedeemed[burnTokenId] = true;
 
-        // Mint the new NFT
-        IMintSpecific(nftContract).mintSpecific(msg.sender, burnTokenId);
 
         // Emit mint event
         emit SeaDropMint(
             nftContract,
-            msg.sender
+            msg.sender,
+            burnTokenId
         );
     }
 }
